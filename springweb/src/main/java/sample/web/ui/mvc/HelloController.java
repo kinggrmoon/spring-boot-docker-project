f725182a69d9083fa3ec33edac07346985e4a40d
grmoon <grmoon@nsmall.com>
2021-07-22 16:52:01 +0900

    first commit

---
+++ springweb/src/main/java/sample/web/ui/mvc/HelloController.java
@@ -0,0 +1,14 @@
+package sample.web.ui.mvc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
---
+++ springweb/src_org/main/java/com/start/springweb/WebRestController.java
@@ -0,0 +1,14 @@
+package com.start.springweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
